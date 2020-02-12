f613e6a936f36b9648e798e48c04e6f78a21f1a0
AhmadGDev <Ahmad.ghelem@gmail.com>
2020-02-12 20:10:49 +0100

    include delete option

--- script.js
+++ script.js
@@ -12,3 +12,7 @@ class Calculator {
         this.previousOperand = ''
         this.operation = undefined
       }
+
+      delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
+      }
