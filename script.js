82b11e8e5a65e8291b7896a7dc0ff37fe553b638
AhmadGDev <Ahmad.ghelem@gmail.com>
2020-02-19 19:59:37 +0100

    put clear all display btn

--- script.js
+++ script.js
@@ -95,3 +95,4 @@ const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
 const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
