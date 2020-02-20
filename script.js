76e0d8e72f80c63f7356e3bc68a26b85a0795dfb
AhmadGDev <Ahmad.ghelem@gmail.com>
2020-02-20 17:01:45 +0100

    put next operand selection

--- script.js
+++ script.js
@@ -99,3 +99,6 @@ const allClearButton = document.querySelector("[data-all-clear]");
 const previousOperandTextElement = document.querySelector(
   "[data-previous-operand]"
 );
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
