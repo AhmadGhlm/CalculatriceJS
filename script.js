1cb28ebfe77537a91895845c7531a7dd35db283f
AhmadGDev <Ahmad.ghelem@gmail.com>
2020-02-20 17:01:20 +0100

    put previous operand selection

--- script.js
+++ script.js
@@ -96,3 +96,6 @@ const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
 const deleteButton = document.querySelector("[data-delete]");
 const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
